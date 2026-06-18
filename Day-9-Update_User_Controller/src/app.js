e7e3dbf3af23da086332caa1974c7e93c7df614f
Eman Nazir <eman406261@gmail.com>
2026-06-18 23:05:53 +0500

    feat(user): add profile management, subscription model & cloudinary delete

---
+++ Day-9-Update_User_Controller/src/app.js
@@ -0,0 +1,27 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { asyncHandler } from "./utils/asyncHandler.js";
+import { ApiError } from "./utils/ApiError.js";
+import { ApiResponse } from "./utils/ApiResponse.js";
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+// configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+// routes import 
+import userRouter from "./routes/user.routes.js"
+// routes declaration 
+app.use("/api/v1/users",userRouter)
+
+
+
+// http://localhost:8000/api/v1/users/register 
+export { app };
