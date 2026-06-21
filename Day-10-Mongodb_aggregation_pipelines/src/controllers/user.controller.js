5b51a2555b2770a4091c1619a5784bc8a55bc552
Eman Nazir <eman406261@gmail.com>
2026-06-21 14:36:38 +0500

    feat: add mongoose import to user controller

--- Day-10-Mongodb_aggregation_pipelines/src/controllers/user.controller.js
+++ Day-10-Mongodb_aggregation_pipelines/src/controllers/user.controller.js
@@ -7,6 +7,7 @@ import {
 } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose"
 const generateAccessAndRefereshTokens = async (userId) => {
   try {
     const user = await User.findById(userId);
--- Day-10-Mongodb_aggregation_pipelines/src/routes/user.routes.js
+++ Day-10-Mongodb_aggregation_pipelines/src/routes/user.routes.js
@@ -32,9 +32,11 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
+
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
 router
