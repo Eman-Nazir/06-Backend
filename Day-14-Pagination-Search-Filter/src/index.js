f4931fd75fc06da7c8811257f0948b33e7789af6
Eman Nazir <eman406261@gmail.com>
2026-06-25 15:22:03 +0500

    feat: add pagination, filtering, sorting and search to video API

---
+++ Day-14-Pagination-Search-Filter/src/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config();
+
+app.on("error", (error) => {
+  console.log(" App Error:", error);
+  throw error;
+});
+
+
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+
+    app.listen(PORT, () => {
+      console.log(` Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(" MongoDB Connection Failed:", err);
+  });
