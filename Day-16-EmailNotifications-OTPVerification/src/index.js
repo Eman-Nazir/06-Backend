f9136277823d0a5785ce586e5af6f47f0e3f7be5
Eman Nazir <eman406261@gmail.com>
2026-06-30 10:36:28 +0500

    feat: add nodemailer email system with OTP verification and password reset

---
+++ Day-16-EmailNotifications-OTPVerification/src/index.js
@@ -0,0 +1,22 @@
+// import dotenv from "dotenv"
+
+import "dotenv/config";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+app.on("error", (error) => {
+  console.log(" App Error:", error);
+  throw error;
+});
+
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+
+    app.listen(PORT, () => {
+      console.log(` Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(" MongoDB Connection Failed:", err);
+  });
